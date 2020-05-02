cbc82a4620f22590f86aa09e62d448dc083c641c
Garrett <geewai.lee@gmail.com>
2020-05-02 02:12:59 -0400

    3 init

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -1,3 +1,11 @@
+// // pass values from a parent component to a child component.
+// A child component can have values handed to it either through
+//  attributes, or through nested content.
+
+// <ChildComponent some-attribute="this gets passed">So does this</ChildComponent>
+
+// https://reactjs.org/docs/components-and-props.html
+
 import React from "react";
 
 export default class TodoBox extends React.Component {
@@ -13,24 +21,48 @@ export default class TodoBox extends React.Component {
 }
 
 class TodoList extends React.Component {
-  // Write code here
   render() {
-    return <div className="todoList">I am a TodoList.</div>;
+    return (
+      <div className="todoList">
+        <table style={{ border: "2px solid black" }}>
+          <tbody>
+            <Todo title="Shopping">Milk</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+// you can get the value of the title attribute
+// set in TodoList
+// (the parent component) by using {this.props.title}.
+// Likewise, you can get the values Milk
+// and 13:00 by using {this.props.children}.
+
+// write some JSX that results in the HTML below.
+class Todo extends React.Component {
+  render() {
+    return (
+      // <tbody>
+      //   <tr>
+      //     <td style="border:1px solid black;">Shopping</td>
+      //     <td style="border:1px solid black;">Milk</td>
+      //   </tr>
+      //   <tr>
+      //     <td style="border:1px solid black;">Hair cut</td>
+      //     <td style="border:1px solid black;">13:00</td>
+      //   </tr>
+      // </tbody>
+      {this.props.title} //gets thte value of the title attribute
+      {this.props.children} // get values of milk and 13:00
+    );
   }
 }
 
 class TodoForm extends React.Component {
-  // Write code here
   render() {
     return <div className="todoForm">I am a TodoForm.</div>;
   }
 }
-
-// Hello React
-// import React from "react";
-
-// export default class TodoBox extends React.Component {
-//   render() {
-//     return <div className="todoBox">Hello, world!</div>;
-//   }
-// }
---
+++ learnyoureact/views/index2.jsx
@@ -0,0 +1,41 @@
+// Implement the missing code above using JSX notation
+// to output the HTML below.
+// Note: you don't have to change server-side code (program.js).
+// Don't forget render and return! :-)
+
+import React from "react";
+
+export default class TodoBox extends React.Component {
+  render() {
+    return (
+      <div className="todoBox">
+        <h1>Todos</h1>
+        <TodoList />
+        <TodoForm />
+      </div>
+    );
+  }
+}
+
+class TodoList extends React.Component {
+  // Write code here
+  render() {
+    return <div className="todoList">I am a TodoList.</div>;
+  }
+}
+
+class TodoForm extends React.Component {
+  // Write code here
+  render() {
+    return <div className="todoForm">I am a TodoForm.</div>;
+  }
+}
+
+// Hello React
+// import React from "react";
+
+// export default class TodoBox extends React.Component {
+//   render() {
+//     return <div className="todoBox">Hello, world!</div>;
+//   }
+// }
